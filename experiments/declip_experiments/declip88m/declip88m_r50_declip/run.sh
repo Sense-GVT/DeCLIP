d5d129a5dab4131fa8001000a9e5b15d9a20486c
zhaolichen <zlc1114@buaa.edu.cn>
2022-06-23 23:15:15 +0800

    feat: code update, add declip88m training & inference configs

---
+++ experiments/declip_experiments/declip88m/declip88m_r50_declip/run.sh
@@ -0,0 +1,7 @@
+mkdir -p petrel.log
+PYTHONPATH=$PYTHONPATH:../../../../ GLOG_vmodule=MemcachedClient=-1 \
+spring.submit run --mpi=pmi2 -n$1 --gpu  --cpus-per-task=6 --job-name $2 $3 $4 $5 $6 \
+"python -u -m prototype.solver.clip_solver --config config.yaml --evaluate"
+# "python -u -m prototype.solver.clip_solver_simsiam_2_view_MLM --config config.yaml --evaluate"
+# --evaluate
+#-x SH-IDC1-10-5-36-[101] 
